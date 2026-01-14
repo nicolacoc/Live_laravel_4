9c1b26623a1b50ef0e71fd62b39eedadc6682e29
Nicola Cocco <niki@motoimco.it>
2026-01-14 16:47:50 +0100

    Add Breeze

--- resources/views/film_list.blade.php
+++ resources/views/film_list.blade.php
@@ -7,6 +7,21 @@
 </head>
 <body>
 <div class="container">
+    <header>
+        @if (Route::has('login'))
+            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+                @auth
+                    <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+    </header>
     <main>
     @foreach($actors as $actor)
         <div class="card mb-3">
