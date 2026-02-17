69fe4589fdc03663ac6cf4daf11aa6e93cab7a6c
Nicola Cocco <niki@motoimco.it>
2026-02-17 16:05:13 +0100

    update home page with background image and styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,11 @@
 <x-app-public_view>
     <x-slot name="head">
-        <style>
-            .sfondo{
-                background-image: url("{{asset('sfondo/IMG_4070.PNG')}}");
-                background-size: cover;
-                min-height: 100vh;
-            }
-        </style>
+        @vite(['resources/css/home.css'])
     </x-slot>
-
-    <div class="sfondo text-white">
-<div class="row">
-    <div class="col-3">
-
+    <div class="sfondo text-white p-0 m-0">
+<div class="row p-3">
+    <div class="col-5">
+        <h1>Benvenuti nell'archivio degli Attori</h1>
     </div>
 </div>
     </div>
