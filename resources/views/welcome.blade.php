e33592cc062166965e2b32a8207a5049d2ff5cf3
Nicola Cocco <niki@motoimco.it>
2026-02-17 16:08:43 +0100

    update home page with background image and styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,10 @@
         @vite(['resources/css/home.css'])
     </x-slot>
     <div class="sfondo text-white p-0 m-0">
-<div class="row p-3">
-    <div class="col-5">
-        <h1>Benvenuti nell'archivio degli Attori</h1>
-    </div>
-</div>
+        <div class="row p-3">
+            <div class="col-5">
+                <h1>Benvenuti nell'archivio degli Attori</h1>
+            </div>
+        </div>
     </div>
 </x-app-public_view>
